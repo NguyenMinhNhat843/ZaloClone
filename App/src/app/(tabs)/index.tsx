684bc2aa4786808f6d84721aaeafb17f93ab78d0
ADMIN-PC\Admin <haubtm699@gmail.com>
2025-03-25 18:39:12 +0700

    chat Item

--- App/src/app/(tabs)/index.tsx
+++ App/src/app/(tabs)/index.tsx
@@ -1,18 +1,328 @@
-import { Link } from "expo-router";
-import React from "react";
-import { Text, View } from "react-native";
+import ChatItem from "@/components/chat/ChatItem";
+import Header from "@/components/Header";
+import React, { useEffect } from "react";
+import { FlatList, Image, StyleSheet, Text, View } from "react-native";
 
-export default function Index() {
+const conversations = [
+  {
+    conversationsId: 1,
+    lastMessage: "Chào bạn",
+    lastMessageOwner: "Đặng Nguyễn",
+    lastMessageTime: "12 giờ",
+    chatName: "Đặng Nguyễn",
+    chatAvatar: require("../../assets/images/avatar1.png"),
+    participants: [
+      {
+        name: "Đặng Nguyễn",
+        avatar: require("../../assets/images/avatar1.png"),
+      },
+      {
+        name: "Bạn",
+        avatar: require("../../assets/images/avatar2.png"),
+      }
+    ],
+  },
+  {
+    conversationsId: 2,
+    lastMessage: "Hôm nay thế nào?",
+    lastMessageOwner: "Bạn",
+    lastMessageTime: "3 phút",
+    chatName: "Nhóm môn công nghệ phần mềm",
+    chatAvatar: null,
+    participants: [
+      {
+        name: "Đặng Nguyễn",
+        avatar: require("../../assets/images/avatar1.png"),
+
+      },
+      {
+        name: "Bạn",
+        avatar: require("../../assets/images/avatar2.png"),
+      }
+      ,
+      {
+        name: "Khôi Nguyễn",
+        avatar: require("../../assets/images/avatar3.png"),
+      }
+    ],
+  },
+  {
+    conversationsId: 3,
+    lastMessage: "OK",
+    lastMessageOwner: "Khôi Nguyễn",
+    lastMessageTime: "T4",
+    chatName: "Nhóm môn công nghệ mới",
+    chatAvatar: require("../../assets/images/background.jpg"),
+    participants: [
+      {
+        name: "Đặng Nguyễn",
+        avatar: require("../../assets/images/avatar1.png"),
+
+      },
+      {
+        name: "Bạn",
+        avatar: require("../../assets/images/avatar2.png"),
+      },
+      {
+        name: "Khôi Nguyễn",
+        avatar: require("../../assets/images/avatar3.png"),
+      },
+      {
+        name: "Huy Nguyễn",
+        avatar: require("../../assets/images/favicon.png"),
+      }
+    ],
+  },
+  {
+    conversationsId: 4,
+    lastMessage: "Đi chơi không anh em?",
+    lastMessageOwner: "Bình Văn",
+    lastMessageTime: "6/3",
+    chatName: "Hội chơi game",
+    chatAvatar: null,
+    participants: [
+      {
+        name: "Nam Hùng",
+        avatar: require("../../assets/images/avatar1.png"),
+
+      },
+      {
+        name: "Bạn",
+        avatar: require("../../assets/images/avatar2.png"),
+      },
+      {
+        name: "Thanh Trần",
+        avatar: require("../../assets/images/avatar3.png"),
+      },
+      {
+        name: "Bình Văn",
+        avatar: require("../../assets/images/avatar1.png"),
+      },
+    ],
+  },
+  {
+    conversationsId: 5,
+    lastMessage: "Chào bạn",
+    lastMessageOwner: "Đặng Nguyễn",
+    lastMessageTime: "12 giờ",
+    chatName: "Đặng Nguyễn",
+    chatAvatar: require("../../assets/images/avatar1.png"),
+    participants: [
+      {
+        name: "Đặng Nguyễn",
+        avatar: require("../../assets/images/avatar1.png"),
+      },
+      {
+        name: "Bạn",
+        avatar: require("../../assets/images/avatar2.png"),
+      }
+    ],
+  },
+  {
+    conversationsId: 6,
+    lastMessage: "Hôm nay thế nào?",
+    lastMessageOwner: "Bạn",
+    lastMessageTime: "3 phút",
+    chatName: "Nhóm môn công nghệ phần mềm",
+    chatAvatar: null,
+    participants: [
+      {
+        name: "Đặng Nguyễn",
+        avatar: require("../../assets/images/avatar1.png"),
+
+      },
+      {
+        name: "Bạn",
+        avatar: require("../../assets/images/avatar2.png"),
+      }
+      ,
+      {
+        name: "Khôi Nguyễn",
+        avatar: require("../../assets/images/avatar3.png"),
+      }
+    ],
+  },
+  {
+    conversationsId: 7,
+    lastMessage: "OK",
+    lastMessageOwner: "Bạn",
+    lastMessageTime: "T4",
+    chatName: "Nhóm môn công nghệ mới",
+    chatAvatar: require("../../assets/images/background.jpg"),
+    participants: [
+      {
+        name: "Đặng Nguyễn",
+        avatar: require("../../assets/images/avatar1.png"),
+
+      },
+      {
+        name: "Bạn",
+        avatar: require("../../assets/images/avatar2.png"),
+      },
+      {
+        name: "Khôi Nguyễn",
+        avatar: require("../../assets/images/avatar3.png"),
+      },
+      {
+        name: "Huy Nguyễn",
+        avatar: require("../../assets/images/favicon.png"),
+      }
+    ],
+  },
+  {
+    conversationsId: 8,
+    lastMessage: "Đi chơi không anh em?",
+    lastMessageOwner: "Bình Văn",
+    lastMessageTime: "6/3",
+    chatName: "Hội chơi game",
+    chatAvatar: null,
+    participants: [
+      {
+        name: "Nam Hùng",
+        avatar: require("../../assets/images/avatar1.png"),
+
+      },
+      {
+        name: "Bạn",
+        avatar: require("../../assets/images/avatar2.png"),
+      },
+      {
+        name: "Thanh Trần",
+        avatar: require("../../assets/images/avatar3.png"),
+      },
+      {
+        name: "Bình Văn",
+        avatar: require("../../assets/images/avatar1.png"),
+      },
+    ],
+  },
+  {
+    conversationsId: 9,
+    lastMessage: "Chào bạn",
+    lastMessageOwner: "Đặng Nguyễn",
+    lastMessageTime: "12 giờ",
+    chatName: "Đặng Nguyễn",
+    chatAvatar: require("../../assets/images/avatar1.png"),
+    participants: [
+      {
+        name: "Đặng Nguyễn",
+        avatar: require("../../assets/images/avatar1.png"),
+      },
+      {
+        name: "Bạn",
+        avatar: require("../../assets/images/avatar2.png"),
+      }
+    ],
+  },
+  {
+    conversationsId: 10,
+    lastMessage: "Hôm nay thế nào?",
+    lastMessageOwner: "Bạn",
+    lastMessageTime: "3 phút",
+    chatName: "Nhóm môn công nghệ phần mềm",
+    chatAvatar: null,
+    participants: [
+      {
+        name: "Đặng Nguyễn",
+        avatar: require("../../assets/images/avatar1.png"),
+
+      },
+      {
+        name: "Bạn",
+        avatar: require("../../assets/images/avatar2.png"),
+      }
+      ,
+      {
+        name: "Khôi Nguyễn",
+        avatar: require("../../assets/images/avatar3.png"),
+      }
+    ],
+  },
+  {
+    conversationsId: 11,
+    lastMessage: "OK",
+    lastMessageOwner: "Bạn",
+    lastMessageTime: "T4",
+    chatName: "Nhóm môn công nghệ mới",
+    chatAvatar: require("../../assets/images/background.jpg"),
+    participants: [
+      {
+        name: "Đặng Nguyễn",
+        avatar: require("../../assets/images/avatar1.png"),
+
+      },
+      {
+        name: "Bạn",
+        avatar: require("../../assets/images/avatar2.png"),
+      },
+      {
+        name: "Khôi Nguyễn",
+        avatar: require("../../assets/images/avatar3.png"),
+      },
+      {
+        name: "Huy Nguyễn",
+        avatar: require("../../assets/images/favicon.png"),
+      }
+    ],
+  },
+  {
+    conversationsId: 12,
+    lastMessage: "Đi chơi không anh em?",
+    lastMessageOwner: "Bình Văn",
+    lastMessageTime: "6/3",
+    chatName: "Hội chơi game",
+    chatAvatar: null,
+    participants: [
+      {
+        name: "Nam Hùng",
+        avatar: require("../../assets/images/avatar1.png"),
+
+      },
+      {
+        name: "Bạn",
+        avatar: require("../../assets/images/avatar2.png"),
+      },
+      {
+        name: "Thanh Trần",
+        avatar: require("../../assets/images/avatar3.png"),
+      },
+      {
+        name: "Bình Văn",
+        avatar: require("../../assets/images/avatar1.png"),
+      },
+    ],
+  },
+]
+
+const Index = () => {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#f4511e"
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Header />
+      <FlatList
+        data={conversations}
+        renderItem={({ item }) => (
+          <ChatItem
+            conversationsId={item.conversationsId}
+            lastMessage={item.lastMessage}
+            lastMessageOwner={item.lastMessageOwner}
+            lastMessageTime={item.lastMessageTime}
+            chatName={item.chatName}
+            chatAvatar={item.chatAvatar}
+            participants={item.participants}
+          />
+        )}
+        keyExtractor={item => item.conversationsId.toString()}
+        showsVerticalScrollIndicator={false}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#f4f3f8",
+  },
+})
+
+export default Index;
---
+++ App/src/components/chat/ChatItem.tsx
@@ -0,0 +1,86 @@
+import { APP_COLOR } from "@/utils/constant"
+import React from "react"
+import { Image, ImageURISource, Pressable, StyleSheet, Text, View } from "react-native"
+
+interface IProps {
+    conversationsId: number,
+    lastMessage: string,
+    lastMessageOwner: string,
+    lastMessageTime: string,
+    chatName: string,
+    chatAvatar: null | ImageURISource,
+    participants: {
+        name: string,
+        avatar: any
+    }[]
+}
+
+const avatarSize = 50
+
+const ChatItem = (props: IProps) => {
+    const { conversationsId, lastMessage, lastMessageOwner,
+        lastMessageTime, chatName, chatAvatar,
+        participants } = props
+
+    return (
+        <Pressable style={styles.container}>
+            <View style={styles.avatar}>
+                {chatAvatar
+                    ? <Image style={styles.avatarImage} source={chatAvatar} />
+                    : <View>
+                        <Image style={styles.avatarImage} source={require('../../assets/images/avatar3.png')} />
+                    </View>
+                }
+            </View>
+            <View style={styles.chat}>
+                <View style={styles.chatInfo}>
+                    <Text numberOfLines={1} style={styles.chatName}>{chatName}</Text>
+                    <Text numberOfLines={1} style={styles.lastMessage}>{lastMessageOwner}: {lastMessage}</Text>
+                </View>
+                <Text style={styles.lastMessageTime}>{lastMessageTime}</Text>
+            </View>
+        </Pressable>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        gap: 20,
+    },
+    avatar: {
+        padding: 10,
+        paddingRight: 0
+    },
+    avatarImage: {
+        width: 50,
+        height: 50,
+        borderRadius: 50,
+    },
+    chat: {
+        flexDirection: 'row',
+        flex: 1,
+        borderBottomWidth: 1,
+        borderColor: "#efe8e8",
+        padding: 10,
+        paddingLeft: 0
+    },
+    chatInfo: {
+        flex: 1,
+        gap: 5
+    },
+    chatName: {
+        fontSize: 15,
+        fontWeight: '400'
+    },
+    lastMessage: {
+        color: '#86898c',
+    },
+    lastMessageTime: {
+        fontSize: 12,
+        textAlign: 'right',
+        marginRight: 10,
+    }
+})
+
+export default ChatItem
