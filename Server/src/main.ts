8ea272e22ba04203e58a4b63783fe75f505ef0c7
NguyenMinhNhat843 <NhatNguyen15062019@gmail.com>
2025-03-29 14:45:26 +0700

    add: lib validator for app

--- Server/src/main.ts
+++ Server/src/main.ts
@@ -1,11 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // Bật CORS cho WebSocket nếu cần
   app.enableCors();
+
+  // 🔹 Kích hoạt validation toàn bộ ứng dụng
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
   await app.listen(process.env.PORT ?? 3000);
 }
